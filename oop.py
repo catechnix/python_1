67131b72b30d3df173e087e0f0bee5bd7f7eaa9a
catechnix <catechnix@yahoo.com>
2023-04-26 09:44:00 -0700

    Create oop.py

---
+++ oop.py
@@ -0,0 +1,21 @@
+"""
+python supports several paradigms:
+imperative/procedural
+functional
+object-oriented
+
+object-oriented programming is a programming paradigm:
+* based on concept of object
+* an object is a bundle of states and behaviour
+* the state is stored in variables known as attributes
+* the behaviour is defined by functions defined within the class, known as methods
+* the type of object is defined by a class
+* the class is used to create several instances
+
+
+
+
+
+
+
+"""
