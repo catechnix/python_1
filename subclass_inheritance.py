68d2663ff5872b7c29d01f9e6ef3491369638715
catechnix <catechnix@yahoo.com>
2023-05-06 11:47:24 -0700

    Create subclass_inheritance.py

---
+++ subclass_inheritance.py
@@ -0,0 +1,21 @@
+###
+Subclass inherits all methods and attributes of their base class
+subclass can implement additional methods and overide inherited methods
+subclasses can inherit from multiple base classes 
+
+subclassing and inheritance
+super()  -- baseclass
+The exception hierarchy
+Defining new exceptions
+
+
+
+
+
+
+
+
+
+
+
+###
