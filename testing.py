5cd4ecff7bf927923d4fe14e3fdbdabe6284e94b
catechnix <catechnix@yahoo.com>
2023-05-15 15:45:13 -0700

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,14 @@
+"""
+Unit testing: 
+Unit testing is the process of verifying that a unit works correctly
+A unit is a function, method, class or module
+The verification is donee through assertions
+An assertion compares an expected result to an actual result obtained by the unit under test
+If the results don't match, the test fails
+A unit test is a piece of code that performs and automates these verifications
+
+Good unit tests should:
+test all possible path
+not depend on or affect the environment
+be runnable on their own
+alway
