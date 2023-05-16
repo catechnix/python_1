99f59e6d983d5cb533c57eb1045317b9a55e5e81
catechnix <catechnix@yahoo.com>
2023-05-16 11:48:38 -0700

    Create api.py

---
+++ api.py
@@ -0,0 +1,33 @@
+"""
+API are ...
+* a way for two pieces of software to talk to each other
+* the interface for software systems
+* sets of requirements that govern how one application can talk to each other
+
+Represental State Transfer (REST)
+* API framework intended to build simpler web services than SOAP
+* Another use for the HTTP protocol
+* Popular due to performance, scale, simplicity and reliability
+* Technically an API framework
+* framework -- allows for the person to have some decision to change, protocol has the same activity, no change
+
+XML-RPC and JSON-RPC:
+* simple framework for communicating over HTTP
+* RPC = Remote procedure call: When one system requests another system to execute code
+* Offer XML and JSON data formats respectivly
+
+What is REST:
+* Representational state transfer
+* API framework built on HTTP
+* APIs often referred to as web services
+* popular due to performance, scale, simplicity and reliability
+Get, Post, Put, Delete -- get info from the server, post new info, put: update info in the server
+
+
+
+
+
+
+
+
+"""
