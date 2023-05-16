fb956fc42d868fad88ada9fe2782156c169c0ce2
catechnix <catechnix@yahoo.com>
2023-05-16 16:54:43 -0700

    Update python_library.py

--- python_library.py
+++ python_library.py
@@ -4,3 +4,21 @@ Where to get libraries:
 *Included with Python itself: datetime, os, system, json
 *from python package index, PyPI: pip install requests
 *Download and install manually: ACI toolkit from github
+
+
+Libraries:
+*Pretty Print
+*Python interpreter Utilities 
+    import sys  ->input arguments
+*Operating System Interfaces:
+    import os  ->how environment variable
+*Date and Time Utilities
+    import datetime
+
+
+
+
+
+
+
+"""
