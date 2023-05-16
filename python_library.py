6093f78fed222f1e183860c362d19d851ae331a3
catechnix <catechnix@yahoo.com>
2023-05-16 16:47:43 -0700

    Create python_library.py

---
+++ python_library.py
@@ -0,0 +1,6 @@
+"""
+Where to get libraries:
+* Write them yourself
+*Included with Python itself: datetime, os, system, json
+*from python package index, PyPI: pip install requests
+*Download and install manually: ACI toolkit from github
