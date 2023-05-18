fa31aace3d3f8470a4fb7aed9a2610f0b38789d5
catechnix <catechnix@yahoo.com>
2023-05-17 17:07:25 -0700

    Create web_framework.py

---
+++ web_framework.py
@@ -0,0 +1,9 @@
+"""
+Web Framework:
+URL routing
+HTML/XML/JSON and other output format templating
+Database manipulation
+Security against Cross-site request forgery and other attacks
+Session storage and retrieval
+
+
