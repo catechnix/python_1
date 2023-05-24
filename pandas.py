4cd5b8f07839dd86a5bb608462dd07dbdaa51baf
catechnix <catechnix@yahoo.com>
2023-05-24 11:34:15 -0700

    Create pandas.py

---
+++ pandas.py
@@ -0,0 +1,22 @@
+"""
+series is columum
+dataFrame is the whole table
+index is the key, can use index to access item in the table
+
+* Load the csv into a DataFrame
+import pandas as pd
+df= pd.read_csv('data.csv')
+print(df.to_string())  //to_string to print the entire DataFrame
+
+pd.options.display.max_rows = 9999 //to change the system max display rows to 9999
+
+JSON=Python Dictionary
+
+If JSON is not in a file, but in a Python dictionary, you can load it into
+a DataFrame directly
+
+
+
+
+
+"""
