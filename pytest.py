56d818b3b3095117485de2bf58f30ce7493063ae
catechnix <catechnix@yahoo.com>
2023-07-14 11:33:19 -0700

    Create pytest.py

---
+++ pytest.py
@@ -0,0 +1,15 @@
+"""
+1. Create a py file, have functions
+2. Test the functions using pytest
+3. create a file  -- import the py file that has all the functions
+4. name the pytest file with prefix test_
+5. run the pytest file with -v 
+6. run py.test  -- can look for file name with test_ 
+7. py.test -v
+
+
+
+
+
+
+"""
