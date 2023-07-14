bde086d776276048b49613ace9bc2496c0327eb3
catechnix <catechnix@yahoo.com>
2023-07-14 13:02:18 -0700

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -6,8 +6,11 @@
 5. run the pytest file with -v 
 6. run py.test  -- can look for file name with test_ 
 7. py.test -v
-
-
+8. in unittest test_function.py, the function also needs to start with test_ prefix
+9. if there is no prefix, have to run using pytest tes_function.py
+10. if just run one test, using pytest test_function.py::test_add -v
+11. if run using the test with the key word -- pytest -v -k "add or string"
+12. if run using the test with the key word -- pytest -v -k "add and string"
 
 
 
